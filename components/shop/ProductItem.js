ff6ea9143da5844a5ca7c367cfa3149b329349fe
Bharat Jain <bharatjain7723@gmail.com>
2020-09-12 19:05:31 +0530

    optimising code

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
+import Card from '../UI/Card';
 
 const ProductItem = props => {
 
@@ -10,7 +11,7 @@ const ProductItem = props => {
     }    
 
     return (
-        <View style={styles.product} >
+        <Card style={styles.product} >
             <View style={styles.touchable}>
                 <TouchableCmp
                     useForeground
@@ -31,22 +32,12 @@ const ProductItem = props => {
                     </View>
                 </TouchableCmp>
             </View>
-        </View>
+        </Card>
     )
 };
 
 const styles = StyleSheet.create({
     product: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
         height: 300,
         margin: 20
     },
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -6,6 +6,7 @@ import CartItem from '../../components/shop/CartItem';
 import Colors from '../../constants/Colors';
 import * as cartActions from '../../store/actions/cart'
 import * as ordersActions from '../../store/actions/orders';
+import Card from '../../components/UI/Card';
 
 const CartScreen = props => {
     const dispatch = useDispatch();
@@ -26,7 +27,7 @@ const CartScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <View style={styles.summary}>
+            <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total: <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) /100}</Text>
                 </Text>
@@ -37,7 +38,7 @@ const CartScreen = props => {
                     onPress={()=>{
                         dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
                     }} />
-            </View>
+            </Card>
             <FlatList
                 data={cartItems}
                 keyExtractor={item => item.productId}
@@ -71,16 +72,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginBottom: 20,
         padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
